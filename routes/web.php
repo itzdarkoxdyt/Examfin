e3faf85685a4b5b78df7560d4a41b3de2a50f0de
Santiago Home <a>
2024-10-24 10:15:41 -0500

    restablecimiento de datos

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('_eventos',App\Http\Controllers\EventoController::class);
-Route::resource('_organizadores',App\Http\Controllers\OrganizadoreController::class);
-Route::resource('_participaciones',App\Http\Controllers\ParticipacioneController::class);
-
