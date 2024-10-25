9658c1bb2a677350f83facea5991e2a2cd25900e
Santiago Home <a>
2024-10-24 23:13:32 -0500

    debido a unos problemas que tuve decidi hacer la autenticacion con blade

---
+++ parcialromor/resources/views/organizadore/show.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ $organizadore->name ?? __('Show') . " " . __('Organizadore') }}
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
+                        <div class="float-left">
+                            <span class="card-title">{{ __('Show') }} Organizadore</span>
+                        </div>
+                        <div class="float-right">
+                            <a class="btn btn-primary btn-sm" href="{{ route('organizadores.index') }}"> {{ __('Back') }}</a>
+                        </div>
+                    </div>
+
+                    <div class="card-body bg-white">
+                        
+                                <div class="form-group mb-2 mb20">
+                                    <strong>Nombre:</strong>
+                                    {{ $organizadore->nombre }}
+                                </div>
+                                <div class="form-group mb-2 mb20">
+                                    <strong>Apellido:</strong>
+                                    {{ $organizadore->apellido }}
+                                </div>
+                                <div class="form-group mb-2 mb20">
+                                    <strong>Email:</strong>
+                                    {{ $organizadore->email }}
+                                </div>
+                                <div class="form-group mb-2 mb20">
+                                    <strong>Telefono:</strong>
+                                    {{ $organizadore->telefono }}
+                                </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
